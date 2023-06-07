4aa3925009a4f740e67e7b7707d5348bab224ba4
Grekov Stanislav <stas.ik1987@yandex.ru>
2023-06-07 22:21:44 +0500

    commit

--- diplom_drf/settings.py
+++ diplom_drf/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework.authtoken',
+    'rest_framework',
     'orders',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- orders/models.py
+++ orders/models.py
@@ -17,6 +17,9 @@ STATE_CHOICES = (
 )
 
 
+
+
+
 class Shop(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     url = models.URLField(verbose_name='Ссылка', null=True, blank=True)
